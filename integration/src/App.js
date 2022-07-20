ff55b45f19e41d01a84d8f69c8e320f8620767e5
jayasurya <jayasuryas@cognitidigital.in>
2022-07-20 15:51:36 -0700

    v3

--- integration/src/App.js
+++ integration/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import User1 from "./User1/index";
-import User2 from "./User2/index";
 
 function App() {
   return (
     <div className="App">
-    <Main />
+      <Main />
     </div>
   );
 }
