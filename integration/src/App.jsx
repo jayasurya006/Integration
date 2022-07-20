7e2ac9d21802528a74dd830c94853f1da37df43e
sivashankar1810 <sivashankard@cognitidigital.in>
2022-07-20 16:55:50 +0530

    v5

--- integration/src/App.jsx
+++ integration/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import MainUi from "./ui/index";
+import { MainUi } from "./ui/index";
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <MainUi />
     </div>
   );
 }
